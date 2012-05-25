37b55ad9a578dd5d3cd27e82d32e09327d065a1f
Whit Morriss <whit@surveymonkey.com>
2012-05-25 12:06:04 -0500

    Extend "packages" resource
     - add pacman handling
    
     - rework to be more data driven (class bag + classmethod pattern)

--- patchwork/packages/__init__.py
+++ patchwork/packages/__init__.py
@@ -2,24 +2,31 @@ from fabric.api import sudo
 from patchwork.info import distro_family
 
 
-def package(*packages):
-    """
-    Installs one or more ``packages`` using the system package manager.
-
-    Specifically, this function calls a package manager like ``apt-get`` or
-    ``yum`` once per package given.
-    """
+class Package(object):
     # Try to suppress interactive prompts, assume 'yes' to all questions
-    apt = "DEBIAN_FRONTEND=noninteractive apt-get install -y %s"
     # Run from cache vs updating package lists every time; assume 'yes'.
-    yum = "yum install -y %s"
-    manager = apt if distro_family() == "debian" else yum
-    for package in packages:
-        sudo(manager % package)
+    managers = dict(apt="DEBIAN_FRONTEND=noninteractive apt-get install -y %s",
+                    yum="yum install -y %s",
+                    pacman='pacman --noconfirm -S %s')
+    distro_map = dict(redhat='yum',
+                      arch='pacman',
+                      debian='apt')
 
+    @classmethod
+    def install(cls, *packages):
+        fam = distro_family()
+        mngr = cls.distro_map.get(fam, None)
+        assert mngr, "No manager for linux distro %s" %fam
+        manager = cls.managers.get(mngr)
+        for package in packages:
+            sudo(manager % package)
+
+
+package = Package.install
 
 def rubygem(gem):
     """
     Install a Rubygem
     """
     return sudo("gem install -b --no-rdoc --no-ri %s" % gem)
+
